08aec0949e762aa0a75c7e5fa929af6f9cec30c2
heilya <123752915+heilya@users.noreply.github.com>
2023-06-04 13:46:16 +0200

    Зробила task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector("#ingredients");
+const creatList = ingredients.map(element => {
+  const ingredientsListItem = document.createElement("li");
+  ingredientsListItem.textContent = element;
+  ingredientsListItem.classList.add("item");
+  ingredientsList.append(ingredientsListItem);
+})
