38d2b6105ada57656077f061242a5d02d388c754
heilya <123752915+heilya@users.noreply.github.com>
2023-06-05 17:08:23 +0200

    Зробила task-10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,31 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const createButton = document.querySelector('button[data-create]');
+const deleteButton = document.querySelector('button[data-destroy]');
+const input = document.querySelector('#controls > input');
+const boxes = document.querySelector("#boxes");
+createButton.addEventListener('click', createBoxes);
+deleteButton.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const userQuantity = input.value;
+  let size = 30;
+  const arrayBoxes = [];
+  for (let i = 1; i <= Number(userQuantity); i += 1) {
+    const colorBoxes = getRandomHexColor();
+    const box = document.createElement('div');
+    size += 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = `${colorBoxes}`;
+    box.style.marginTop = "10px";
+    arrayBoxes.push(box);
+  }
+  boxes.append(...arrayBoxes)
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+  input.value = "";
+
+}
