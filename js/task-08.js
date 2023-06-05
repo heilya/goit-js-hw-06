92eb9a7ba5e9b85508aaafe1541ef4a2895f7347
heilya <123752915+heilya@users.noreply.github.com>
2023-06-05 15:56:46 +0200

    Зробила task-08

--- js/task-08.js
+++ js/task-08.js
@@ -2,4 +2,14 @@ const formEl = document.querySelector(".login-form");
 formEl.addEventListener("submit", onSubmit);
 function onSubmit(evt) {
     evt.preventDefault();
+    const { email, password } = evt.currentTarget.elements;
+    const data = {
+        email: email.value,
+        password: password.value,
+    }
+    if (email.value === "" || password.value === "") {
+        alert("All fields are required to be completed!")
+    }
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    evt.currentTarget.reset();
 }
